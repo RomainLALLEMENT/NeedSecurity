91244db41a78956f0c60b2ae542d1852950f5ae9
Leonard Jouen <leonardjouen@gmail.com>
2022-01-10 13:01:35 +0100

    merge dev

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 require ('inc/pdo.php');
 require ('inc/fonctions.php');
 
+$val = 'c0a8014a';
+debug($val);
+
+echo hexadecimalCipher($val);
 include('inc/header.php');
+
 ?>
-<h1></h1>
+    <h1></h1>
 
 <?php
 include('inc/footer.php');
