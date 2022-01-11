d448defa82b4bcc8557475aaa1f866df30d307b7
Leonard Jouen <leonardjouen@gmail.com>
2022-01-11 09:06:11 +0100

    back footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
 <script src="./assets/js/main.js"></script>
 </body>
 </html>
