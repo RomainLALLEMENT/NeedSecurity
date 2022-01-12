65ba0967b2d94f78038f61ff0e4781ff2fbf22b2
anthonychevalier <contactpro.antho@gmail.com>
2022-01-12 09:54:45 +0100

    d minuscule du nom en majuscule, + beau :zap:

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,5 @@
 <?php
-$NOM_SITE = 'NƎƎd Security';
+$NOM_SITE = 'NƎƎD Security';
 $NOM_SITE_COLORED = '<span class="logo_col_1">NƎƎd</span> <span class="logo_col_2">Security</span>';
 
 $DB_HOST = 'localhost';
