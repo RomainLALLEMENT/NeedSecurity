88f189a0bfb97489e514a7acde646535ea42d7b1
Leonard Jouen <leonardjouen@gmail.com>
2022-01-13 18:23:09 +0100

    :hammer: main back

--- dashboard/assets/js/main_back.js
+++ dashboard/assets/js/main_back.js
@@ -1,4 +1,3 @@
-
 $( document ).ready(function() {
     const container = $('#container');
 
@@ -20,7 +19,15 @@ $( document ).ready(function() {
                 'Blue',
                 'Yellow'
             ],
-            hoverOffset: 4
+            datasets: [{
+                label: 'My First Dataset',
+                data: [300, 50, 100],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'],
+                hoverOffset: 4
+            }]
         }
 
         let config = {
